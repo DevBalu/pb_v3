22fb5fd8c8e2d1e573fe454813cb2dab712508b2
DevBalu <ghenov.andrei@gmail.com>
2017-02-08 23:18:40 +0200

    update sql

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php  
 	$con = new mysqli("localhost", "root", "", "pb");
-	//$con = new mysqli("mysql.hostinger.co.uk", "u699553655_andy", "popamamonta", "u699553655_pb");
+	// $con = new mysqli("mysql.hostinger.co.uk", "u699553655_andy", "popamamonta", "u699553655_pb");
 
 	if(mysqli_connect_errno()){
 		printf("Connect failed: %s\n", mysqli_connect_error());
